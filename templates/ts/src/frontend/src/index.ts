e53622a090c0409385ef496e50fe1f55c25a76f2
johanneskuhs <johannes.kuhs@reactorone.com>
2022-04-25 14:25:32 -0400

    v0.0.8; Replaced rollup with webpack; Use of module federation; Export init function in index.ts;

--- templates/ts/src/frontend/src/index.ts
+++ templates/ts/src/frontend/src/index.ts
@@ -1,4 +1,6 @@
-import { YoStack } from "@yostack/sdk-frontend-react";
+import {AppSetupContext} from "@yostack/sdk-frontend-react";
 import {ExampleComponent} from "./components/example.component";
 
-YoStack.registerComponent('{{appName}}:example-component', ExampleComponent);
+export function init(context: AppSetupContext) {
+    context.registerComponent('{{appName}}:example-component', ExampleComponent);
+}
